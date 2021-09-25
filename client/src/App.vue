d504539543a42725caddc44924b257414222a341
TheRolf <yann.lv@live.fr>
2021-09-25 21:09:45 +0200

    Added minimal stuff for conversion

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App', 
+  components: {}
 }
 </script>
 
